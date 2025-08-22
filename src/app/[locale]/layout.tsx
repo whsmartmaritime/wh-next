feb0c1a5a10c9a349bb0432fabef7f0adc5d3e86
neo <admin@wheelhouse.vn>
2025-08-23 01:52:43 +0700

    setup next-intl

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,16 @@
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+ 
+export default async function LocaleLayout({
+  children,
+  params
+}: LayoutProps<'/[locale]'>) {
+  // Ensure that the incoming `locale` is valid
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+ 
+  // ...
+}
