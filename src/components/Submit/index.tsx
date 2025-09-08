86415d4e8439ddf50b8fbff21021da3901c79e78
neo <admin@wheelhouse.vn>
2025-09-08 22:50:57 +0700

    tạo Submit

---
+++ src/components/Submit/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Button from "../Button";
+import type { ButtonProps } from "../Button";
+import React from "react";
+
+export type SubmitProps = ButtonProps & {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit" | "reset";
+};
+
+export default function Submit({
+  children,
+  className,
+  arrowRotation = 0,
+  onClick,
+  type = "button",
+  ...rest
+}: SubmitProps) {
+  // Không dùng href, chỉ dùng cho submit hoặc button có onClick
+  return (
+    <Button className={className} arrowRotation={arrowRotation} {...rest}>
+      <span
+        onClick={onClick}
+        // Đảm bảo không ảnh hưởng hiệu ứng flip
+        style={{ display: "contents" }}
+      >
+        {children}
+      </span>
+    </Button>
+  );
+}
