9a7228bcadeac6daf86edc6ce8bc27db37c2f295
neo <admin@wheelhouse.vn>
2025-09-24 16:04:24 +0700

    hover và thêm link cho card

--- src/app/[locale]/solutions/page.tsx
+++ src/app/[locale]/solutions/page.tsx
@@ -5,6 +5,7 @@ import { PageHero } from "@/components/PageHero";
 import { BackgroundGrid } from "@/components/BackgroundGrid";
 import { BackgroundScanline } from "@/components/BackgroundScanline";
 import Image from "next/image";
+import Link from "next/dist/client/link";
 export async function generateMetadata(props: {
   params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
@@ -102,31 +103,44 @@ export default async function SolutionsPage(props: {
       {/* solution list section */}
       <section className="relative container-gutter bg-neutral-200 text-neutral-900">
         <BackgroundGrid />
-        <div className="grid grid-cols-12 items-stretch ">
+        <div className="grid grid-cols-12 items-stretch">
           {(["item1", "item2", "item3", "item4"] as const).map((key, i) => (
             <div
               className="col-span-12 lg:col-span-6 flex flex-col mb-16"
               key={i}
             >
-              <div className="flex flex-col md:flex-row ">
-                <div className="relative w-full md:w-1/2 aspect-[16/10]">
-                  <Image
-                    src={t.raw(`solutionList.items.${key}.imgSrc`)}
-                    alt={t(`solutionList.items.${key}.imgAlt`)}
-                    width={1600}
-                    height={1000}
-                    className="object-cover"
-                  />
+              <Link
+                href={t(`solutionList.items.${key}.href`)}
+                className="group block relative overflow-hidden transition-all duration-500"
+              >
+                {/* Component scanline - ẩn mặc định, hiện khi hover */}
+                <BackgroundScanline
+                  crosshairs="all"
+                  className="absolute inset-0 boder boder-neutral-500 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none z-0 "
+                  opacity={0.1}
+                />
+
+                {/* Nội dung card */}
+                <div className="flex flex-col md:flex-row relative z-10">
+                  <div className="relative w-full md:w-1/2 aspect-[16/10]">
+                    <Image
+                      src={t.raw(`solutionList.items.${key}.imgSrc`)}
+                      alt={t(`solutionList.items.${key}.imgAlt`)}
+                      width={1600}
+                      height={1000}
+                      className="object-cover"
+                    />
+                  </div>
+                  <div className="w-full md:w-1/2 group-hover:border group-hover:border-neutral-500/20 group-hover:border-b-neutral-700">
+                    <h2 className="text-xl xl:text-2xl font-bold mx-4 mt-4">
+                      {t(`solutionList.items.${key}.title`)}
+                    </h2>
+                    <p className="text-lg xl:text-xl line-clamp-4 m-4">
+                      {t(`solutionList.items.${key}.intro`)}
+                    </p>
+                  </div>
                 </div>
-                <div className="w-full md:w-1/2 ">
-                  <h2 className="text-xl xl:text-2xl font-bold mx-4">
-                    {t(`solutionList.items.${key}.title`)}
-                  </h2>
-                  <p className="text-lg xl:text-xl line-clamp-4 m-4">
-                    {t(`solutionList.items.${key}.intro`)}
-                  </p>
-                </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
