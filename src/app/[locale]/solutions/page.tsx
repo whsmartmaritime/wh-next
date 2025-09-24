dbb4e81e21ecede0e00567e75c4e5e948c768108
neo <admin@wheelhouse.vn>
2025-09-24 15:26:48 +0700

    thêm card cho solutions

--- src/app/[locale]/solutions/page.tsx
+++ src/app/[locale]/solutions/page.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { routing } from "@/i18n/routing";
 import { PageHero } from "@/components/PageHero";
-
+import { BackgroundGrid } from "@/components/BackgroundGrid";
+import { BackgroundScanline } from "@/components/BackgroundScanline";
+import Image from "next/image";
 export async function generateMetadata(props: {
   params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
@@ -75,6 +77,59 @@ export default async function SolutionsPage(props: {
           ctaPrimary={t("hero.ctaPrimary")}
           ctaSecondary={t("hero.ctaSecondary")}
         />
+      </section>
+      {/** section overview **/}
+
+      <section className="relative bg-neutral-200 text-neutral-900">
+        <BackgroundGrid />
+        <div className="container-gutter py-16 lg:py-32">
+          <div className="relative bg-neutral-200 w-full h-full items-center justify-center  border border-neutral-500/20 pb-16">
+            <BackgroundScanline
+              crosshairs="all"
+              className="absolute inset-0 "
+              opacity={0.1}
+            />
+            <h2 className="sr-only">{t("overview.title")}</h2>
+            <p className="uppercase tracking-[0.25em] opacity-95 font-bold py-8">
+              {t("overview.intro")}
+            </p>
+            <div className="text-sm sm:text-lg lg:text-4xl text-justify mx-[calc(var(--gutter-h))]">
+              <p className="block mb-8">{t("overview.desc")}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* solution list section */}
+      <section className="relative container-gutter bg-neutral-200 text-neutral-900">
+        <BackgroundGrid />
+        <div className="grid grid-cols-12 items-stretch ">
+          {(["item1", "item2", "item3", "item4"] as const).map((key, i) => (
+            <div
+              className="col-span-12 lg:col-span-6 flex flex-col mb-16"
+              key={i}
+            >
+              <div className="flex flex-col md:flex-row ">
+                <div className="relative w-full md:w-1/2 aspect-[16/10]">
+                  <Image
+                    src={t.raw(`solutionList.items.${key}.imgSrc`)}
+                    alt={t(`solutionList.items.${key}.imgAlt`)}
+                    width={1600}
+                    height={1000}
+                    className="object-cover"
+                  />
+                </div>
+                <div className="w-full md:w-1/2 ">
+                  <h2 className="text-xl xl:text-2xl font-bold mx-4">
+                    {t(`solutionList.items.${key}.title`)}
+                  </h2>
+                  <p className="text-lg xl:text-xl line-clamp-4 m-4">
+                    {t(`solutionList.items.${key}.intro`)}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   );
