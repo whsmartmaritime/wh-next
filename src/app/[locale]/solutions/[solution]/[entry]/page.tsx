c8a088c2e9e91c8b9fb1df3f4015ba0c11a031c2
neo <admin@wheelhouse.vn>
2025-10-09 02:25:32 +0700

    phát triển entry

--- src/app/[locale]/solutions/[solution]/[entry]/page.tsx
+++ src/app/[locale]/solutions/[solution]/[entry]/page.tsx
@@ -3,6 +3,8 @@ import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
 import { routing } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
+import Image from "next/image";
 
 export function generateStaticParams() {
   // Lấy toàn bộ path dạng /solutions/[solution]/[entry]
@@ -95,6 +97,7 @@ export default async function EntryPage(props: {
   params: Promise<{ locale: string; solution: string; entry: string }>;
 }) {
   const { locale, solution, entry } = await props.params;
+  const t = await getTranslations("entry");
 
   const raw = await fs.readFile(
     path.join(
@@ -112,14 +115,77 @@ export default async function EntryPage(props: {
   const { default: Entry } = await import(
     `@/content/${locale}/solutions/${solution}/${entry}.mdx`
   );
+
   return (
     <>
-      <h1>{frontmatter.title}</h1>
-      {frontmatter.description}
-
-      <article className="prose max-w-none">
-        <Entry />
-      </article>
+      {/** BgGrid */}
+      <div
+        aria-hidden="true"
+        className="fixed inset-0 container-gutter grid grid-cols-12 h-full pointer-events-none select-none"
+      >
+        <div className="col-start-1 border-l border-neutral-500/20 h-full" />
+        <div className="col-start-3 border-l border-neutral-500/20 h-full hidden lg:block" />
+        <div className="col-start-7 border-l border-neutral-500/20 h-full hidden md:block" />
+        <div className="col-start-11 border-l border-neutral-500/20 h-full hidden lg:block" />
+        <div className="col-start-12 border-r border-neutral-500/20 h-full" />
+      </div>
+      <div className="container-gutter grid grid-cols-12">
+        {/** Cột 1 */}
+        <div className="col-span-2 hidden lg:block flex flex-col">
+          <div className="border-y border-neutral-500/20 flex flex-col py-8">
+            <div className="text-xl md:text-2xl font-semibold ml-4 lg:ml-8 mb-2">
+              {t("author")}
+            </div>
+            <div className="text-muted-foreground self-center">
+              {frontmatter.author}
+            </div>
+          </div>
+          <div className="border-y border-neutral-500/20 flex flex-col py-8">
+            <div className="text-xl md:text-2xl font-semibold ml-4 lg:ml-8 mb-2">
+              {t("publishedAt")}
+            </div>
+            <div className="text-muted-foreground self-center">
+              {frontmatter.publishedAt}
+            </div>
+          </div>
+          <div className="border-y border-neutral-500/20 flex flex-col py-8">
+            <div className="text-xl md:text-2xl font-semibold ml-4 lg:ml-8 mb-2">
+              {t("updatedAt")}
+            </div>
+            <div className="text-muted-foreground self-center">
+              {frontmatter.updatedAt ?? frontmatter.publishedAt}
+            </div>
+          </div>
+          <div className="border-y border-neutral-500/20 flex flex-col py-8">
+            <div className="text-xl md:text-2xl font-semibold ml-4 lg:ml-8 mb-2">
+              {t("category")}
+            </div>
+            <div className="text-muted-foreground self-center">
+              {frontmatter.category}
+            </div>
+          </div>
+        </div>
+        {/** Cột 2 */}
+        <div className="col-span-12 lg:col-span-8 lg:col-start-4">
+          <article className="  flex flex-col items-start justify-center py-8 lg:py-16">
+            <h1 className="text-4xl lg:text-6xl font-bold mb-8 lg:mb-16">
+              {frontmatter.meta.title}
+            </h1>
+            <div className="aspect-[16/9] w-[124.5%] lg:-translate-x-1/10">
+              <Image
+                src={frontmatter.meta.ogImage}
+                alt={frontmatter.meta.title}
+                fill
+                className="object-cover"
+                priority
+              />
+            </div>
+            <div className="w-full">
+              <Entry />
+            </div>
+          </article>
+        </div>
+      </div>
     </>
   );
 }
