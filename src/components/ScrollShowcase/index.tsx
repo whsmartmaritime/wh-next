a3ac57ecc75f015be09616f791f593dbb9be67fa
neo <admin@wheelhouse.vn>
2025-10-18 00:37:22 +0700

    response cho scroll show case

--- src/components/ScrollShowcase/index.tsx
+++ src/components/ScrollShowcase/index.tsx
@@ -47,32 +47,47 @@ export default function ScrollShowcase({
   }, [items.length]);
 
   return (
-    <div className={`relative grid grid-cols-12 ${className}`}>
-      <div className="col-span-4">
+    <div className={`relative grid grid-cols-1 lg:grid-cols-12 ${className}`}>
+      <div className="lg:col-span-4 order-1">
         {items.map((item, index) => (
           <div
             key={index}
             ref={(el) => {
               textRefs.current[index] = el;
             }}
-            className="h-screen flex items-center justify-center"
+            className="lg:h-screen flex items-center justify-center"
           >
-            <div className="">
-              <div className="text-4xl font-bold mb-4">{item.title}</div>
+            <div className="flex flex-col gap-4 lg:gap-8">
+              <div className="text-4xl font-bold">{item.title}</div>
               <div>{item.description}</div>
+              <div className="relative w-full aspect-[16/9] lg:hidden">
+                <BackgroundScanline />
+                <Image
+                  key={index}
+                  src={item.image.src}
+                  alt={item.image.alt}
+                  fill
+                  priority={index === 0}
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  className="object-cover transition-opacity duration-300 p-8"
+                  style={{
+                    opacity: activeIndex === index ? 1 : 0,
+                  }}
+                />
+              </div>
             </div>
           </div>
         ))}
       </div>
 
       {/* Pinned Image Container */}
-      <div className="sticky top-0 h-screen col-span-7 col-start-6 flex items-center justify-end relative">
+      <div className="sticky top-0 h-screen lg:col-span-7 lg:col-start-6 flex items-center justify-end relative order-2 hidden lg:block">
         <BackgroundScanline
           opacity={0.1}
           className="inset-auto w-6/7 h-full"
           crosshairs="all"
         />
-        <div className="relative w-full aspect-[4/3] max-h-full">
+        <div className="relative w-full aspect-[4/3] ">
           {items.map((item, index) => (
             <Image
               key={index}
