1c7d180fbb328b56421c9229886ad8c2d6723113
neo <admin@wheelhouse.vn>
2025-10-18 01:09:33 +0700

    khi có 1 ảnh ở hero thì ảnh đó nằm chính giữa

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -31,66 +31,77 @@ export default function Hero({
   isHome = false,
 }: HeroProps) {
   return (
-    <div className={`relative ${className}`}>
-      <div className="grid grid-cols-1 lg:grid-cols-12 items-center">
-        <div className="col-span-3 flex flex-col gap-8">
-          <div className="text-3xl lg:text-5xl xl:text-7xl font-semibold leading-tight tracking-tight text-balance w-full lg:w-[150%]">
-            {title}
+    <div
+      className={`relative grid grid-cols-12 gap-8 items-center ${className}`}
+    >
+      <div className="col-span-12 lg:col-span-3 flex flex-col gap-8">
+        <div className="text-3xl lg:text-5xl xl:text-7xl font-semibold leading-tight tracking-tight text-balance w-full lg:w-[150%]">
+          {title}
+        </div>
+        <div className="text-xl lg:text-2xl font-medium text-muted-foreground">
+          {subtitle}
+        </div>
+        {ctas.length > 0 && (
+          <div className="relative flex flex-col sm:flex-row lg:flex-col">
+            {ctas.map((cta, i) => (
+              <Button
+                key={i}
+                href={cta.href}
+                className={
+                  "w-full min-h-20  border-t border-b border-neutral-500/20 focus:ring-white" +
+                  (isHome
+                    ? " text-white hover:bg-white hover:text-black"
+                    : " text-black hover:bg-black hover:text-white")
+                }
+              >
+                {cta.label}
+              </Button>
+            ))}
           </div>
-          <div className="text-xl lg:text-2xl font-medium text-muted-foreground">
-            {subtitle}
-          </div>
-          {ctas.length > 0 && (
-            <div className="relative flex flex-col sm:flex-row lg:flex-col">
-              {ctas.map((cta, i) => (
-                <Button
-                  key={i}
-                  href={cta.href}
-                  className={
-                    "w-full min-h-20  border-t border-b border-neutral-500/20 focus:ring-white" +
-                    (isHome
-                      ? " text-white hover:bg-white hover:text-black"
-                      : " text-black hover:bg-black hover:text-white")
-                  }
-                >
-                  {cta.label}
-                </Button>
-              ))}
+        )}
+      </div>
+
+      <div className="relative col-span-12 lg:col-span-7 lg:col-start-6 ">
+        <div className="relative w-full lg:w-[calc(var(--column)*10)] aspect-[16/9] ">
+          {images.fore && (
+            <div
+              className={
+                "absolute top-0 right-0 z-10 lg:w-[calc(var(--column)*8.5)] h-auto   border border-white/5 rounded-lg backdrop-blur-2xl shadow-[0px_3rem_4rem_1rem_rgba(0,0,0,0.5)] overflow-hidden " +
+                (images.back ? "w-[calc(var(--column)*10)]" : "w-full")
+              }
+            >
+              <div className=" relative w-full rounded-md overflow-hidden aspect-[16/9]">
+                <Image
+                  src={images.fore.src}
+                  alt={images.fore.alt}
+                  fill
+                  sizes="(min-width:1280px) 750px, (min-width:1024px) 600px, 70vw"
+                  className="object-cover"
+                  priority
+                />
+              </div>
             </div>
           )}
-        </div>
 
-        <div className="relative col-span-7 col-start-6 ">
-          <div className="relative w-full lg:w-[calc(var(--column)*10)] aspect-[16/9] ">
-            <div className="absolute top-0 right-0 z-10 w-[calc(var(--column)*10)] lg:w-[calc(var(--column)*8.5)] h-auto   border border-white/5 rounded-lg backdrop-blur-2xl shadow-[0px_3rem_4rem_1rem_rgba(0,0,0,0.5)] overflow-hidden">
-              {images.fore && (
-                <div className=" relative w-full rounded-md overflow-hidden aspect-[16/9]">
-                  <Image
-                    src={images.fore.src}
-                    alt={images.fore.alt}
-                    fill
-                    sizes="(min-width:1280px) 750px, (min-width:1024px) 600px, 70vw"
-                    className="object-cover"
-                    priority
-                  />
-                </div>
-              )}
-            </div>
-            {images.back && (
-              <div className="absolute bottom-0 left-0 z-0 w-[calc(var(--column)*10)] lg:w-[calc(var(--column)*8.5)] h-auto border border-white/5 rounded-lg backdrop-blur-2xl shadow-[0px_3rem_4rem_1rem_rgba(0,0,0,0.5)] overflow-hidden ">
-                <div className="relative w-full rounded-md border border-white/5 bg-gray-900 shadow-[0px_0rem_0.25rem_0rem_rgba(0,0,0,0.5)] overflow-hidden aspect-[16/9]">
-                  <Image
-                    src={images.back.src}
-                    alt={images.back.alt}
-                    fill
-                    sizes="(min-width:1280px) 520px, (min-width:1024px) 420px, 60vw"
-                    className="object-cover"
-                    priority
-                  />
-                </div>
+          {images.back && (
+            <div
+              className={
+                "absolute bottom-0 left-0 z-0 lg:w-[calc(var(--column)*8.5)] h-auto border border-white/5 rounded-lg backdrop-blur-2xl shadow-[0px_3rem_4rem_1rem_rgba(0,0,0,0.5)] overflow-hidden " +
+                (images.fore ? "w-[calc(var(--column)*10)]" : "w-full")
+              }
+            >
+              <div className="relative w-full rounded-md border border-white/5 bg-gray-900 shadow-[0px_0rem_0.25rem_0rem_rgba(0,0,0,0.5)] overflow-hidden aspect-[16/9]">
+                <Image
+                  src={images.back.src}
+                  alt={images.back.alt}
+                  fill
+                  sizes="(min-width:1280px) 520px, (min-width:1024px) 420px, 60vw"
+                  className="object-cover"
+                  priority
+                />
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
