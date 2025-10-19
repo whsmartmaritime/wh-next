5b9d3535c7351e10029f570d1b006bee6a88b7ea
neo <admin@wheelhouse.vn>
2025-10-19 15:54:43 +0700

    cập nhật CSP

--- next.config.ts
+++ next.config.ts
@@ -35,23 +35,8 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: `
-                default-src 'self';
-                script-src 'self' 'strict-dynamic' https://www.googletagmanager.com https://www.google-analytics.com https://maps.googleapis.com https://maps.gstatic.com;
-                style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-                font-src 'self' https://fonts.gstatic.com;
-                img-src 'self' data: https: blob:;
-                connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com;
-                frame-src 'self' https://www.google.com;
-                child-src 'self' https://www.google.com;
-                object-src 'none';
-                base-uri 'self';
-                form-action 'self';
-                frame-ancestors 'none';
-                upgrade-insecure-requests;
-              `
-              .replace(/\s{2,}/g, " ")
-              .trim(),
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://maps.googleapis.com https://maps.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: blob:; connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com; frame-src 'self' https://www.google.com; child-src 'self' https://www.google.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;",
           },
         ],
       },
