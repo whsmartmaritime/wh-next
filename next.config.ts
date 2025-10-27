5d0b2284e30c2593638d73bd1db237e5945a4f1c
neo <admin@wheelhouse.vn>
2025-10-27 16:42:03 +0700

    gỡ bỏ remark-fronmatter để tránh xung đột turbopact

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,12 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import createMDX from "@next/mdx";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [
-      // Parse math first so $$ .. $$ becomes math nodes before other transforms
-      remarkMath,
-      remarkFrontmatter,
-      [remarkMdxFrontmatter, { name: "frontmatter" }],
-    ],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [],
+    rehypePlugins: [],
   },
 });
 
