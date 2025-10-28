9b4362a83084dc8652a0c3f442934085ee26c8ed
neo <admin@wheelhouse.vn>
2025-10-29 04:14:41 +0700

    bỏ routing và navigation của next-intl

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 	htmlLimitedBots: /.*/,
 };
-
-export default createNextIntlPlugin()(nextConfig);
+export default nextConfig;
