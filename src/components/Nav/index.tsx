c19dd219b8892d89e702984c9ea549d92b495343
neo <admin@wheelhouse.vn>
2025-10-29 02:58:16 +0700

    chỉnh một chút về e-navigation

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -43,10 +43,10 @@ export default async function NavMenu({ locale }: NavProps) {
 					</li>
 					<li>
 						<Link
-							href={`/${locale}${commonMessages.nav.solutions.items.eNavigation.href}`}
+							href={`/${locale}${commonMessages.nav.solutions.items['e-navigation'].href}`}
 							className="block px-4 py-2 hover:bg-gray-100"
 						>
-							{commonMessages.nav.solutions.items.eNavigation.label}
+							{commonMessages.nav.solutions.items['e-navigation'].label}
 						</Link>
 					</li>
 				</ul>
