89dcf7215cd1c36fe31b153f19fac0673ba4564f
neo <admin@wheelhouse.vn>
2025-10-28 17:56:23 +0700

    khôi phục lại request.ts và next-intl config để chuyển đổi dần

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
@@ -25,4 +26,4 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default nextConfig;
+export default createNextIntlPlugin()(nextConfig);
