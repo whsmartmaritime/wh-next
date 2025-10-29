abcbe2a614e036048c0f16530d0a8300c1f30bdb
neo <admin@wheelhouse.vn>
2025-10-29 07:56:10 +0700

    thêm mặc định compiler cho react

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	reactCompiler: true,
 	htmlLimitedBots: /.*/,
 };
 export default nextConfig;
