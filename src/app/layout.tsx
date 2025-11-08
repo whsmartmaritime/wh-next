33de34964d1d1e51122bab3fb79a2d4e7719dffa
neo <admin@wheelhouse.vn>
2025-11-09 04:49:52 +0700

    bỏ viewport, để kích hoạt build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,7 @@ import '@/styles/globals.css';
 import 'katex/dist/katex.min.css';
 import type { Viewport } from 'next';
 
-export const viewport: Viewport = {
-	themeColor: [
-		{ media: '(prefers-color-scheme: light)', color: '#ffffff' },
-		{ media: '(prefers-color-scheme: dark)', color: '#0a0a0a' },
-	],
-};
+
 
 export default async function RootLayout({
 	children,
